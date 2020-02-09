e0fc060ad7d409d6bde21491cba70a4b46d459f5
Lambda_School_Loaner_264 <lambda_school_loaner_264@Lambda-School-Loaner-264s-MacBook-Pro.local>
2020-02-09 14:10:17 -0500

    Added get functionality

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // implement your API here
+const express = require('express')
+
+const server = express();
+
+const Hubs = require('./data/db.js')
+
+//Create
+
+
+//Read
+server.get('/api/users', (req, res) => {
+    // read data from database
+    Hubs.find() // return a promise
+     .then(hubs => {
+         res.status(200).json(hubs)
+     })
+     .catch( error => {
+         console.log(error)
+         res.status(500).json({ Error: "Error getting the list of users"})
+     })
+})
+
+// Update
+
+
+//Delete
+
+const port = 5000;
+server.listen(port, () => console.log(`\n ** Port is Listening on: ${port} ** \n`))
