33e9b3813f54a663708c0d590286d7012d035fce
Lambda_School_Loaner_264 <lambda_school_loaner_264@Lambda-School-Loaner-264s-MacBook-Pro.local>
2020-02-09 20:19:24 -0500

    users can create a user now

--- index.js
+++ index.js
@@ -3,21 +3,40 @@ const express = require('express')
 
 const server = express();
 
-const Hubs = require('./data/db.js')
+server.use(express.json())
+
+const Users = require('./data/db.js')
+
 
 //Create
 
+server.post('/api/users', (req, res) => {
+    const userData = req.body;
+    
+    
+    if(!userData.name || !userData.bio) {
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+    } else {Users.insert(userData)
+    .then(user => {
+        res.status(201).json(user)
+    })
+    .catch( error => {
+        console.log(error);
+
+        res.status(500).json({ errorMessage: "There was an error while saving the user to the database" })
+    })}
+})
 
 //Read
 server.get('/api/users', (req, res) => {
     // read data from database
-    Hubs.find() // return a promise
-     .then(hubs => {
-         res.status(200).json(hubs)
+    Users.find() // return a promise
+     .then(users => {
+         res.status(200).json(users)
      })
      .catch( error => {
          console.log(error)
-         res.status(500).json({ Error: "Error getting the list of users"})
+         res.status(500).json({ errorMessage: "The users information could not be retrieved."})
      })
 })
 
